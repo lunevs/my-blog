7b8f16dd3b6164694053102ede8fd02361bc34bb
Sergey Lunev <lunev.s@gmail.com>
2022-03-16 01:40:47 +0300

    refactoring code 1

--- src/App.js
+++ src/App.js
@@ -20,12 +20,37 @@ const Button = ({text, onClick}) => (
     </button>
 )
 
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>the app is used by pressings the buttons</div>
+        )
+    }
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
 
 const App = () => {
     const [counter, setCounter] = useState(0);
+    const [clicks, setClicks] = useState({
+        left: 0,
+        right: 0
+    })
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setClicks({...clicks, left: clicks.left + 1})
+    }
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setClicks({...clicks, right: clicks.right + 1})
+    }
 
     const t = new Date()
-    const a = 4
 
     const increaseByOne = () => setCounter(counter + 1)
     const decreaseByOne = () => setCounter(counter - 1)
@@ -33,14 +58,21 @@ const App = () => {
 
     return (
         <div>
-            <Hello name="Ivan" age={5 + 7} />
-            <Hello name="John" age={a * 5}/>
-            <Hello name="April" age="0" />
             <p>It's now {t.toString()}</p>
             <Display counter={counter} />
-            <Button onClick={increaseByOne} text="add one" />
-            <Button onClick={decreaseByOne} text="minus one" />
+            <Button onClick={increaseByOne} text="increaseByOne" />
+            <Button onClick={decreaseByOne} text="decreaseByOne" />
             <Button onClick={setToZero} text="set to zero" />
+            <br />
+            <br />
+            <br />
+
+            {clicks.left}
+            <Button onClick={handleLeftClick} text="left" />
+            <Button onClick={handleRightClick} text="right" />
+            {clicks.right}
+
+            <History allClicks={allClicks} />
         </div>
     )
 }
